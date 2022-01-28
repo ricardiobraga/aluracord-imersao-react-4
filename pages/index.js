a1ee6c6333f7daaa5ab61505e2f0cf16974e126a
Ricardo Braga <rcardiobraga@gmail.com>
2022-01-28 13:37:22 -0300

    Projeto finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import React, { useState } from 'react'
+
+import { useRouter } from 'next/router'
+
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
 
@@ -70,34 +74,7 @@ import appConfig from '../config.json';
 
 
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -129,15 +106,18 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'ricardiobraga';
+  //const username = 'ricardiobraga';
+  const [username, setUserName] = useState('')
+  const roteramento = useRouter()
 
+  const gitLogo = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
   return (
     <>
-      <GlobalStyle />
+      
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.primary[700],
           backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
@@ -160,6 +140,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={ function (info) {
+                info.preventDefault()
+                roteramento.push(`/chat?username=${username}`)
+                console.log("send")
+              }
+            }
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -170,7 +156,14 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
-            <TextField
+            {/* <input type="text" value={username} onChange={ e => setUserName(e.target.value)} /> */}
+            
+            <TextField 
+              value={username}
+              onChange={ e => {
+                
+                setUserName(e.target.value)}
+              }
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -217,7 +210,8 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={username.length < 2 ? gitLogo :  `https://github.com/${username}.png` }
+              
             />
             <Text
               variant="body4"
